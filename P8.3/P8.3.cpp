e4865fbdffcda7cf38a67560932d0ce4f107b140
RAY OTTO <rjotto123@gmail.com>
2023-10-25 11:17:13 -0400

    completed romanCharValue function.

--- P8.3/P8.3.cpp
+++ P8.3/P8.3.cpp
@@ -1,1 +1,57 @@
- =
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+
+int romanCharValue(char r);
+
+
+int main(void) {
+	int numeral = 0;
+	string input;
+	char r;
+
+	while (true) {
+		cout << "Enter a roman value or Q to quit: ";
+		cin>> r;
+		if (input == "Q") break;
+		numeral= romanCharValue(r);
+		cout << r <<  " = " <<numeral<< endl;
+	}
+	return 0;
+}
+
+int romanCharValue(char r) {
+	int numeral = 0;
+		if (r == 'I')
+		{
+			numeral += 1;
+		}
+		if (r == 'V')
+		{
+			numeral += 5;
+		}
+		if (r == 'X')
+		{
+			numeral += 10;
+		}
+		if (r == 'L')
+		{
+			numeral += 50;
+		}
+		if (r == 'C')
+		{
+			numeral += 100;
+		}
+		if (r == 'D')
+		{
+			numeral += 500;
+		}
+		if (r == 'M')
+		{
+			numeral += 1000;
+		}
+	
+	return numeral;
+}
